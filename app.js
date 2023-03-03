bd1e6683210bcd87acf3c14b6b74a567a83bb595
Yohannes Bekele <yohannesbekele81@gmail.com>
2023-03-03 17:06:45 +0300

    updated

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 require('express-async-errors')
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./db/connect')
 const app = express();
+
 const musicRouter = require('./routes/songRoutes');
 const statRouter = require('./routes/statRoute');
-
+const bodyParser = require('body-parser');
 app.use(express.json())
 app.get('', (req, res) => {
     res.send('Addis Software project')
-})
+});
 app.use(cors())
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/musics', musicRouter);
 app.use('/stats', statRouter);
